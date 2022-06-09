d683279899cbf3faef8b1db4aa51861644e16e08
defacube <85292886+yoyeet961@users.noreply.github.com>
2022-06-09 23:59:26 +1100

    Create fill.sh
    could be buggy

---
+++ fill.sh
@@ -0,0 +1,12 @@
+echo "Are you sure? If so, please wait 30 seconds..."
+read -r -t 30 value
+apt install python
+apt install python2
+apt install python3
+apt install figlet
+apt install nano
+apt install toilet
+apt install sl
+apt install openssl
+apt install openssl-tool
+apt install tor
